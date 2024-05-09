dc188df0b46260b3dc47acdf868f4f33cd1ac838
Sandesh Jain <68385781+Jain-Sandesh@users.noreply.github.com>
2024-05-09 15:42:04 +0530

    Update ArrayQ3.java

--- Array/ArrayQ3.java
+++ Array/ArrayQ3.java
@@ -4,29 +4,30 @@ package Array;
 
 import java.util.Scanner;
 
-public class ArrayQ3 {
-    
-    public static void main(String[] args) {
-        
+class InnerArrayQ3
+{
+    public void inputElements(int size, int num[])
+    {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the size of array : ");
-        int size = sc.nextInt();
-        int num[]=new int[size];
-
-        //input
         System.out.println("Enter the elements of array :-");
         for(int i=0; i<size; i++)
         {
             num[i] = sc.nextInt();
         }
 
-        //print the array
+    }
+
+    public void displayArray(int size, int num[])
+    {
         System.out.println("Given array is :-");
         for(int i=0; i<size; i++)
         {
             System.out.print("{ " + num[i] + " }");
         }
+    }
 
+    public void findMinAndMaxElement(int size, int num[])
+    {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
@@ -48,5 +49,26 @@ public class ArrayQ3 {
         System.out.println();
         System.out.println("Largest number in the given array is : " + max);
         System.out.println("Smallest number in the given array is : " + min);
+    }
+}
+
+public class ArrayQ3 {
+    
+    public static void main(String[] args) {
+        
+        InnerArrayQ3 a3 = new InnerArrayQ3();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
+        int num[]=new int[size];
+
+        //input
+        a3.inputElements(size, num);
+        
+        //print the array
+        //a3.displayArray(size, num);
+
+        a3.findMinAndMaxElement(size, num);        
     }
 }
