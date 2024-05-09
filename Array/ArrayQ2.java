8ee72291c22e21508a954d59f76a4f8987d0b347
Sandesh Jain <68385781+Jain-Sandesh@users.noreply.github.com>
2024-05-09 15:41:29 +0530

    Update ArrayQ2.java

--- Array/ArrayQ2.java
+++ Array/ArrayQ2.java
@@ -4,27 +4,57 @@ package Array;
 
 import java.util.Scanner;
 
+/**
+ * InnerArrayQ2
+ */
+class InnerArrayQ2 
+{
+    public void inputNames(int size, String names[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the names with space between each name :- ");
+        for(int i=0; i<size; i++)
+        {
+            names[i] = sc.next();
+        }
+    }
+
+    public void displayNames(int size, String names[])
+    {
+        System.out.println("Names present in array is :- ");
+
+        System.out.print("{ ");
+        for(int i=0; i<size; i++)
+        {
+            System.out.print(names[i] + ", ");
+        }
+        System.out.print("}.");
+
+        //Different type of display
+        //  //output
+        //  for(int i=0; i<size; i++)
+        //  {
+        //      System.out.println("Name " + (i+1) + " is : " + names[i] + ".");
+        //  }
+    }
+    
+}
+
 public class ArrayQ2 {
     
     public static void main(String[] args) {
         
+        InnerArrayQ2 a2 = new InnerArrayQ2();
+
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the size of array : ");
         int size = sc.nextInt();
 
         String names[]=new String[size];
 
-        //input
-        System.out.print("Enter the names with space between each name :- ");
-        for(int i=0; i<size; i++)
-        {
-            names[i] = sc.next();
-        }
-
-        //output
-        for(int i=0; i<size; i++)
-        {
-            System.out.println("Name " + (i+1) + " is : " + names[i] + ".");
-        }
+        a2.inputNames(size, names);
+        a2.displayNames(size, names);
+        
     }
 }
