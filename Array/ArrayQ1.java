014992486b30f4ab6a15e55459d873628f498b00
Sandesh Jain <68385781+Jain-Sandesh@users.noreply.github.com>
2024-05-09 15:40:57 +0530

    Update ArrayQ1.java

--- Array/ArrayQ1.java
+++ Array/ArrayQ1.java
@@ -4,21 +4,22 @@ package Array;
 
 import java.util.Scanner;
 
-public class ArrayQ1 {
-    
-    public static void main(String[] args) {
-        
+class InnerArrayQ1
+{
+    public void inputElements(int size, int a[])
+    {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the size of array : ");
-        int size = sc.nextInt();
-        int a[]=new int[size];
-
         System.out.println("Enter the numbers of array");
         //input
         for(int i=0; i<size; i++)
         {
             a[i] = sc.nextInt();
         }
+    }
+
+    public void findNum(int size, int a[])
+    {
+        Scanner sc=new Scanner(System.in);
 
         System.out.print("Enter the number to be search : ");
         int x = sc.nextInt();
@@ -28,12 +29,29 @@ public class ArrayQ1 {
         {
             if(a[i] == x)
             {
-                System.out.println("Number is found at index : " + i + ".");
+                System.out.println("Number is found at index : " + (i+1) + ".");
                 flag = true;
             }
         }
         if (flag == false) {
             System.out.println("Number is not present in the given array.");
         }
+    }
+}
+
+public class ArrayQ1 {
+    
+    public static void main(String[] args) {
+        
+        InnerArrayQ1 a1 = new InnerArrayQ1();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
+        int a[]=new int[size];
+
+        a1.inputElements(size, a);
+        a1.findNum(size, a);
+        
     }
 }
